805f94082b91712a30982e23969d8be0344f7a98
LofA <contact.lofa@gmail.com>
2024-07-03 00:05:55 +0700

    chore: restructuring file and folder

---
+++ src/assets/svgs/appointment/index.ts
@@ -0,0 +1,8 @@
+import BannerReprescription from './bnr_prompt_represcription.svg';
+
+import { ReactComponent as CloseIcon } from './ico_appointment_close.svg';
+import { ReactComponent as EndIcon } from './ico_appointment_end.svg';
+import { ReactComponent as FewIcon } from './ico_appointment_few.svg';
+import { ReactComponent as OpenIcon } from './ico_appointment_open.svg';
+
+export { BannerReprescription, CloseIcon, EndIcon, FewIcon, OpenIcon };
