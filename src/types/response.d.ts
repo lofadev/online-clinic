e5c80546938873729bb7b6f638f23d67c8455c58
LofA <contact.lofa@gmail.com>
2024-07-22 12:35:44 +0700

    chore: fix error when run build

--- src/types/response.d.ts
+++ src/types/response.d.ts
@@ -1,4 +1,7 @@
+export type ResponseCode = 200 | 500 | 401 | 404 | 402 | 403;
+
 interface IMeta {
+  language: 'JP' | 'EN';
   success: 1 | 0;
   status: boolean;
   error?: string | null;
@@ -6,6 +9,7 @@ interface IMeta {
   code: ResponseCode;
   message: string;
 }
+
 export interface IResponse<T> {
   meta: IMeta;
   data: T;
